a6c0190d47e1182002d22e842e066da1d9ab5158
MarinichevAnton <102620701+MarinichevAnton@users.noreply.github.com>
2022-03-29 21:39:12 +0300

    Finish Project

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// Переменные
+//var name = 'Anton'
+const firstName = 'Anton'
+const age = 26
+const inProgrammer = true
+
+//name = 'Ant'
+
+
+//console.log ('имя:'+'firstName +', а возраст: 'age')
+//alert ('имя:firstName +', а возраст: 'age')
+
+const lastname = prompt ('Ведите фамилию')
+alert (firstName + ' ' + lastname)
+
